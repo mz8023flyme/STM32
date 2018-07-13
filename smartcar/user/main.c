31794fd06e69e00e673727c7081e26d4e8d7a9d9
mz8023flyme <2421174533@qq.com>
2018-07-13 21:56:12 +0800

    dance

--- smartcar/user/main.c
+++ smartcar/user/main.c
@@ -12,38 +12,30 @@ int main(void)
         usart_init();
         TIM8_PWM_Init();
         
-//        int i;
+
 ////                printf("[%s][%d]\r\n", __func__, __LINE__);
         while(1)
         {
-                
-                
-                
-                M1_Revolve(180);
-                M2_Revolve(-20);
-//                MOTOR2_PIN2=1;
-//                MOTOR2_PIN1=0;
-//                MOTOR1_PIN2=0;
-//                MOTOR1_PIN1=1;
-//                TIM8->CCR4=100;
-//                delay_ms(1000);
-//                TIM8->CCR1=5;
-//                delay_ms(1000);
-//                TIM8->CCR4=0;
-//                TIM8->CCR1=20;
-//                M1_Revolve(1);
-//                M2_Revolve(1);
-//                
-//                TIM8->CCR1=20;
-//                delay_ms(2000);
-//////                M1_Revolve(-199);
-//////                M2_Revolve(-199);
-////                TIM8->CCR1=10;
-//                TIM8->CCR2=100;
-//                TIM8->CCR4=150;
-//                delay_ms(2000);
-//                delay_ms(1000);
-//                M1_Revolve(100);
-//                delay_ms(1000);
+                TIM8->CCR1=15;
+                M1_Revolve(50);
+                M2_Revolve(50);
+                delay_ms(500);
+                M1_Revolve( 0 );
+                M2_Revolve( 0 );
+                delay_ms(500);
+                TIM8->CCR1=10;
+                M1_Revolve(-50);
+                M2_Revolve(-50);
+                delay_ms(500);
+                M1_Revolve( 0 );
+                M2_Revolve( 0 );
+                delay_ms(500);
+                TIM8->CCR1=20;
+                M1_Revolve(50);
+                M2_Revolve(50);
+                delay_ms(500);
+                TIM8->CCR1=15;
+                M1_Revolve( 0 );
+                M2_Revolve( 0 );
         }
 }
